d363fa7cc2c7f84d55a911bcad94b723b2e805a6
HenPrado <44711096+HenPrado@users.noreply.github.com>
2023-03-03 04:15:22 -0300

    Update mintWithAllowlist.ts

--- cmv3-demo-initialization/mintWithAllowlist.ts
+++ cmv3-demo-initialization/mintWithAllowlist.ts
@@ -57,8 +57,8 @@ const mintingWallet = metaplex.identity().publicKey;
       guard: "allowList",
       group: "10",
       settings: {
-        path: "proof",
-        merkleProof: "86553717b69b1370209256c8f4c440fd3ff7c7055a49fc8465858f4ffd2c282b",
+        path: "86553717b69b1370209256c8f4c440fd3ff7c7055a49fc8465858f4ffd2c282b",
+        merkleProof: merkleProof,
       },
     }),
   ];
